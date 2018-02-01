49041424b72b75ac66fc69e2aa393db29b3431e8
Mohammad Farman Abbasi <moabbasi@BLR.ALLEGISINDIA.COM>
2018-02-01 17:06:49 +0530

    ng5module

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+
+
+@NgModule({
+  imports: [MatButtonModule,MatToolbarModule,MatCardModule],
+  exports: [MatButtonModule,MatToolbarModule,MatCardModule],
+})
+export class MaterialModule { }
