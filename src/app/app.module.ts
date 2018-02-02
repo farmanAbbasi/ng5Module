0931c305869a8266e44be24081fc9a97fa42317a
Mohammad Farman Abbasi <moabbasi@BLR.ALLEGISINDIA.COM>
2018-02-02 16:44:03 +0530

    added json in the file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import{HttpClientModule} from '@angular/common/http';//
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
